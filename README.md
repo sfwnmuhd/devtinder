0cd98a08b083eaf38a34b416aec4e4367a940900
Muhammad Safwan P K <safwanmuhammad3099@gmail.com>
2025-08-04 16:18:34 +0300

    vite react setup done

---
+++ README.md
@@ -0,0 +1,6 @@
+# DevTinder
+
+
+- Create a Vite + React application
+- Remove unneccessary code and create a Hello World app
+- Install Tailwind CSS
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      
+      <h1 className='text-5xl text-red-500 font-bold'>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
